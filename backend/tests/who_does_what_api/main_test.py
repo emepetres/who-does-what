ba880975c0d2dc68512ce933c2c5feea6ff955c8
Javi Carnero <javier@carnero.net>
2026-01-04 14:22:36 +0100

    🎉(api) python base project
    - Scaffolding with python best practices
    - CI pipeline

---
+++ backend/tests/who_does_what_api/main_test.py
@@ -0,0 +1,8 @@
+import pytest
+from who_does_what_api.main import hello_world
+
+
+@pytest.mark.parametrize("input_string", ["hello world", "hello1, world2"])
+def test_hello_world(input_string):
+    upper_string = hello_world(input_string)
+    assert upper_string == input_string.upper()
